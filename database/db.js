1f794e6c52a7e13954b519d4a8dcdf3b16749a3f
kamau-David <dayvieek@gmail.com>
2026-03-09 02:18:31 +0300

    initial commit

---
+++ database/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const connectToDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Mongodb connected successfully");
+  } catch (error) {
+    console.error("Database connection failed", error);
+    process.exit(1);
+  }
+};
+module.exports = connectToDb;
