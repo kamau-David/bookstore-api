d5ad39771f1975fc7d45ecc6ba43bc2f07d47c78
kamau-David <dayvieek@gmail.com>
2026-03-09 23:34:54 +0300

    importing routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const connectToDb = require("./database/db");
+const bookRoutes = require("./routes/book-routes");
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,5 +11,8 @@ connectToDb();
 
 //use middleware
 app.use(express.json());
+
+//routes
+app.use("/api/routes", bookRoutes);
 
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
