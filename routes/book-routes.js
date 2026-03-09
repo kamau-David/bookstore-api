ba2cd45c99072054167fc4266ceb2b4f58341ee3
kamau-David <dayvieek@gmail.com>
2026-03-09 23:17:47 +0300

    routing controllers

---
+++ routes/book-routes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const router = express.Router()
+
+//creating routes
+router.get('/get',  )
+router.get('/get/:id',  )
+router.post('/add',  )
+router.put('/update/:id',  )
+router.delete('/delete/:id',  )
